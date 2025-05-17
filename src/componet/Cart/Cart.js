72c9778cdcc873065b24799c105d4c04fd8c1a27
M.Mubeen Taveer <88052224+MubeenTanveer@users.noreply.github.com>
2025-05-17 16:49:22 +0500

    Improvments

--- src/componet/Cart/Cart.js
+++ src/componet/Cart/Cart.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./Cart.css";
 import CharBar from "./CartBar";
 const Cart = (props) => {
-  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-  const totalMaximum=Math.max(...dataPointValues);
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoints) => (
         <CharBar
-          key={dataPoints.lable}
           value={dataPoints.value}
           maxValue={totalMaximum}
-          label={dataPoints.lable}
+          key={dataPoints.label}
+          label={dataPoints.label}
         />
       ))}
     </div>
--- src/componet/NewExpense/NewExpense.js
+++ src/componet/NewExpense/NewExpense.js
@@ -2,10 +2,6 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
   const newExpenseHandler = (expense) => {
-    const expenseData = {
-      ...expense,
-    };
-    console.log(expense);
     props.onAddExpense(expense);
   };
   return (
